61bafa9f9b283e9e4e046fc2c7e5645d48a930c5
mb21 <mb21@users.noreply.github.com>
2018-09-20 14:32:28 +0200

    strip yaml

--- src/React/Basic/CommonmarkRenderer.js
+++ src/React/Basic/CommonmarkRenderer.js
@@ -2,8 +2,18 @@
 
 var parser   = new require("commonmark").Parser();
 var renderer = new require("commonmark-react-renderer")();
+var yamlFront = require('yaml-front-matter');
 
 exports.renderMd = function(md) {
-  var ast = parser.parse(md);
-  return renderer.render(ast);
+  var results;
+  try {
+    results = yamlFront.safeLoadFront(md)
+  } catch (e) {
+    results = {__content: md};
+  }
+  var ast = parser.parse(results.__content)
+    , els = renderer.render(ast)
+    ;
+  // delete results.__content;
+  return els;
 };
--- src/React/Basic/CommonmarkRenderer.purs
+++ src/React/Basic/CommonmarkRenderer.purs
@@ -2,4 +2,5 @@ module React.Basic.CommonmarkRenderer where
 
 import React.Basic as React
 
+-- TODO: return YAML, maybe using purescript-argonaut-core
 foreign import renderMd :: String -> Array React.JSX
