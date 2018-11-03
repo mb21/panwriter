381e676a196e7aea4b9dd259f31b6bfa7f795258
mb21 <mb21@users.noreply.github.com>
2018-11-03 13:31:30 +0100

    add fix-path package

--- src/js/rendererPreload.js
+++ src/js/rendererPreload.js
@@ -1,6 +1,9 @@
 // Node.js APIs are available in this preload process.
 
+const fixPath = require('fix-path');
+
 process.once('loaded', () => {
+  fixPath();
   global.appMain = require('../../output/AppRenderer');
   require('./Exporter');
 });
