d5f85683871fad6df0eac4d9037194b5d57b485b
mb21 <mb21@users.noreply.github.com>
2018-11-26 14:30:34 +0100

    move things to markdown-it-pandoc

--- src/React/Basic/PreviewRenderer.js
+++ src/React/Basic/PreviewRenderer.js
@@ -2,36 +2,7 @@
 
 var ipcRenderer = require('electron').ipcRenderer
   , Document    = require('../../src/js/Document')
-  , katex       = require('katex')
-  ;
-var md = require('markdown-it')()
-           .use( require('markdown-it-bracketed-spans') )
-           // TODO: sanitize attrs (at least keys with `on*` and vals with `javascript:*`, see https://github.com/arve0/markdown-it-attrs#security
-           .use( require('markdown-it-attrs') )
-           .use( require('markdown-it-container'), 'dynamic', {
-                // adapted from https://github.com/markdown-it/markdown-it-container/issues/23
-                validate: function() { return true; },
-                render: function(tokens, idx, options, env, slf) {
-                  var token     = tokens[idx]
-                    , className = token.info.trim()
-                    , renderedAttrs = slf.renderAttrs(token)
-                    ;
-                  if (token.nesting === 1) {
-                    return (className && className !== '{}')
-                             ? '<div class="' + className + '">'
-                             : '<div' + renderedAttrs + '>'
-                             ;
-                  } else {
-                    return '</div>';
-                  }
-                }
-              })
-           .use( require('markdown-it-deflist') )
-           .use( require('markdown-it-footnote') )
-           .use( require('markdown-it-implicit-figures'), {figcaption: true} )
-           .use( require('markdown-it-sub') )
-           .use( require('markdown-it-sup') )
-           .use( require('markdown-it-texmath').use(katex) )
+  , md          = require('markdown-it-pandoc')()
   ;
 
 var renderInProgress = false
