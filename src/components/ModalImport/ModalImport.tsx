33da875fefcbedcb191fe4876897fc5dbd0f7b07
mb21 <mb21@users.noreply.github.com>
2022-05-01 15:25:29 +0200

    more

--- src/components/ModalImport/ModalImport.tsx
+++ src/components/ModalImport/ModalImport.tsx
@@ -2,12 +2,17 @@ import { useEffect, useReducer } from 'react'
 
 import { ImportOpts } from '../../options'
 import { Button } from '../Button/Button'
+import { EditorKv } from '../EditorKv/EditorKv'
 
 import styles from './ModalImport.module.css'
 
 type Action = {
   type: 'setFromFormat';
   fromFormat: string;
+} | {
+  type: 'setFromOpt';
+  key: string;
+  value: string;
 }
 
 const reducer = (state: ImportOpts, action: Action): ImportOpts => {
@@ -15,6 +20,12 @@ const reducer = (state: ImportOpts, action: Action): ImportOpts => {
     case 'setFromFormat': {
       const { fromFormat } = action
       return { ...state, fromFormat }
+    }
+    case 'setFromOpt': {
+      const { key, value } = action
+      const { fromOpts } = state
+      fromOpts[key] = value
+      return { ...state, fromOpts }
     }
   }
 }
@@ -50,15 +61,33 @@ export const ModalImport = () => {
 
   return (
     <div className={styles.root}>
-      <p className={styles.text}>Which format do you want to import from?</p>
-      <select
-        autoFocus
-        value={importOpts.fromFormat}
-        onChange={e => dispatch({type: 'setFromFormat', fromFormat: e.target.value })}
-        >
-        {formats.map(({name, value}) =>
-          <option value={value} key={value}>{name}</option>) }
-      </select>
+      <h4>Input format</h4>
+      <div className='kvs'>
+        <select
+          autoFocus
+          value={importOpts.fromFormat}
+          onChange={e => dispatch({type: 'setFromFormat', fromFormat: e.target.value })}
+          >
+          {formats.map(({name, value}) =>
+            <option value={value} key={value}>{name}</option>) }
+        </select>
+      </div>
+
+      <h4>Input options</h4>
+      <div className='kvs'>
+        {fromOptsKvs.map(kv =>
+          <EditorKv
+            key={kv.name}
+            kv={kv}
+            value={importOpts.fromOpts[kv.name] || ''}
+            setKv={(key, value) => dispatch({ type: 'setFromOpt', key, value })}
+          />)}
+      </div>
+
+      <h4>Markdown output extensions</h4>
+      <div className='kvs'>
+      </div>
+
       <Button onClick={() => submit(importOpts)} variant='primary'>Import</Button>
       <Button onClick={() => cancel()} variant='secondary'>Cancel</Button>
     </div>
--- src/options.ts
+++ src/options.ts
@@ -1,9 +1,24 @@
+import { Kv } from "./components/EditorKv/EditorKv";
+
 export interface ImportOpts {
   fromFormat: string | 'docx' | 'xml';
   fromOpts: {
     'track-changes': 'accept' | 'reject' | 'all';
     'extract-media': boolean;
-    wrap: 'auto' | 'none' | 'preserve';
   };
   toExtensions: Record<string, boolean>;
 }
+
+export const fromOptsKvs: Kv[] = [{
+  name: 'track-changes'
+, label: 'Docx track changes'
+, type: 'select'
+, options: [
+    { label: 'Accept proposed changes', value: 'accept' }
+  , { label: 'Reject proposed changes', value: 'reject' }
+  ]
+}, {
+  name: 'extract-media'
+, label: 'Docx extract images and media to folder'
+, type: 'checkbox'
+}]
